392d29455809a018b89bb434bc2a0d4b7ff9518c
ClaRa-git <rameau.claire.cr@gmail.com>
2025-04-08 14:15:15 +0200

    Ajout d'une méthode de connexion pour react

--- www/src/Controller/SecurityController.php
+++ www/src/Controller/SecurityController.php
@@ -2,14 +2,24 @@
 
 namespace App\Controller;
 
+use App\Repository\ProfileRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class SecurityController extends AbstractController
 {
+    private ProfileRepository $profileRepository;
+
+    public function __construct(ProfileRepository $profileRepository)
+    {
+        $this->profileRepository = $profileRepository;
+    }
+
     #[Route(path: '/', name: 'app_index')]
     public function index(): RedirectResponse
     {
@@ -35,5 +45,47 @@ class SecurityController extends AbstractController
     public function logout(): void
     {
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
+    }
+
+    /**
+     * @Route("/login-react", name="app_login_react")
+     */
+    #[Route(path: '/login-react', name: 'app_login_react')]
+    public function loginReact(Request $request): JsonResponse
+    {
+        // on récupère la requête
+        $data = json_decode($request->getContent(), true);
+
+        // on récupère le username et le mot de passe
+        $username = $data['username'];
+        $password = $data['password'];
+        // on récupère l'user en bdd d'après le username
+        $user = $this->profileRepository->findOneBy(['username' => $username]);
+
+        // on vérifie si l'utilisateur existe
+        if (!$user) {
+            return new JsonResponse([
+                'success' => false,
+                'error' => 'Utilisateur non trouvé',
+                'message' => 'Utilisateur non trouvé',
+            ], Response::HTTP_UNAUTHORIZED);
+        }
+        // on vérifie si le mot de passe est correct
+        if ($password !== $user->getPassword()) {
+            return new JsonResponse([
+                'success' => false,
+                'error' => 'Mot de passe invalide',
+                'message' => 'Mot de passe invalide'
+            ], Response::HTTP_UNAUTHORIZED);
+        }
+
+        return new JsonResponse([
+            'success' => true,
+            'message' => 'Connexion réussie',
+            'user' => [
+                'id' => $user->getId(),
+                'username' => $user->getUsername(),
+            ]
+        ], Response::HTTP_OK);
     }
 }
