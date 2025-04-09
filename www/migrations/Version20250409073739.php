bd9eadbdf9e713df6be9eb6bba8fda1acc1c6471
Claire Rameau <rameau.claire.cr@gmail.com>
2025-04-09 11:17:57 +0200

    Ajout de l'icone ai

--- www/migrations/Version20250409073739.php
+++ www/migrations/Version20250409073739.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20250407085923 extends AbstractMigration
+final class Version20250409073739 extends AbstractMigration
 {
     public function getDescription(): string
     {
